c5e870d182f8d77da3825f1bdabfedb584724a0d
Denis Camargo <denis.camargo@silveiracontabilidade.com.br>
2026-02-03 13:53:57 -0300

    para producao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://10.0.0.7:8200';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://10.0.0.6:8200';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
